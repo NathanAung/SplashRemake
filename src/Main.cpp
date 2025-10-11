2a5c49a15dbab1a6d9eca46b37011cc8dc68a4b7
hakesi6221 <yuanjianren000@gmail.com>
2025-10-11 18:46:18 +0900

    update 1011

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,80 @@
 ﻿# include <Siv3D.hpp>
 
+class Player{
+	private:
+		enum State{
+			None,		// 例外用
+			Solid,		// 固体
+			Liquid,		// 液体
+			Gas,		// 気体
+		};
+		const float m_moveForce = 150;		// 移動速度
+		const float m_jumpForce = 1500;		// ジャンプ時に加わる力
+		const float m_maxHP = 100;			// 最大HP
+		const Vec2 m_cameraOffset{ 0, -300 };	// カメラの座標補正値
+		const float m_cameraRatio = 0.5;
+		P2World* m_world;		// P2ワールド
+		P2Body m_collider;		// 物理物体の当たり判定
+		Texture m_sprite;		// プレイヤーの見た目のテクスチャ
+		Camera2D m_camera;		// 2D カメラ
+		State m_state;
+
+		void MoveHorizontal(Vec2 velocity){
+			if (KeyD.pressed()){
+				m_collider.setVelocity( Vec2{ 1 * m_moveForce, velocity.y } );
+			}
+			if (KeyA.pressed()){
+				m_collider.setVelocity( Vec2{ -1 * m_moveForce, velocity.y } );
+			}
+			if (KeyD.up() || KeyA.up()){
+				m_collider.setVelocity( Vec2{ 0 * m_moveForce, velocity.y } );
+			}
+		}
+
+		void Jump(){
+			if (KeySpace.down()){
+				m_collider.applyLinearImpulse( Vec2{ 0, m_jumpForce });
+			}
+		}
+
+	public:
+		/// @brief 
+		/// @param world P2World
+		/// @param firstPos 
+		Player(P2World* world, Vec2 firstPos){
+			m_state = Solid;
+			m_world = world;
+			const Image image{ U"../App/Assets/Sprites/Player/slime.png" };
+			m_sprite = Texture(image.scaled(0.1));
+			m_collider = m_world->createRect(P2Dynamic, firstPos, image.scaled(0.06).size(), P2Material{ 1.0f, 0, 0, 1.0f});
+			m_collider.setFixedRotation(true);
+			m_camera = { firstPos + m_cameraOffset, m_cameraRatio };
+		}
+
+		void Update(){
+			// m_position = m_collider.getPos();
+			Print << U"State " << m_state << U"\n";
+			Vec2 velocity = m_collider.getVelocity();
+			MoveHorizontal(velocity);
+			// Jump();
+			m_camera.setCenter(m_collider.getPos() + m_cameraOffset);
+		}
+
+		void Draw(Array<P2Body> bodies){
+			// m_camera.update();
+			{
+				const auto t = m_camera.createTransformer();
+				m_sprite.drawAt(m_collider.getPos());
+				// すべての地面を描画する
+				for (const auto& body : bodies)
+				{
+					body.draw(Palette::Gray);
+				}
+			}
+			m_camera.draw(Palette::Orange);
+		}
+};
+
 void Main()
 {
 	Window::Resize(1280, 720);
@@ -13,21 +88,20 @@ void Main()
 	// 2D 物理演算のワールド
 	P2World world;
 
+	constexpr Vec2 m_firstPos{ 0, 0 };
+
+	Player m_player(&world, m_firstPos);
 	// 地面
 	Array<P2Body> grounds;
-	grounds << world.createRect(P2Static, Vec2{ 0, -200 }, SizeF{ 600, 20 });
-	grounds << world.createLine(P2Static, Vec2{ 0, 0 }, Line{ -500, -150, -300, -50 });
-	grounds << world.createLineString(P2Static, Vec2{ 0, 0 }, LineString{ Vec2{ 100, -50 }, Vec2{ 200, -50 }, Vec2{ 600, -150 } });
-
-	Array<P2Body> bodies;
-
-	// 2D カメラ（中心座標 (0, -300), 拡大率 1.0）
-	Camera2D camera{ Vec2{ 0, -300 }, 1.0 };
+	grounds << world.createRect(P2Static, Vec2{ m_firstPos.x, m_firstPos.y + 300 }, SizeF{ 2000, 20 });
+	grounds << world.createRect(P2Static, Vec2{ m_firstPos.x, m_firstPos.y + 100 }, SizeF{ 100, 20 });
+	// grounds << world.createLine(P2Static, Vec2{ 0, 0 }, Line{ -500, -150, -300, -50 });
+	// grounds << world.createLineString(P2Static, Vec2{ 0, 0 }, LineString{ Vec2{ 100, -50 }, Vec2{ 200, -50 }, Vec2{ 600, -150 } });
 
 	while (System::Update())
 	{
 		ClearPrint();
-		Print << U"bodies.size(): " << bodies.size() << U"\n";
+		// Print << U"bodies.size(): " << bodies.size() << U"\n";
 
 		for (accumulatedTime += Scene::DeltaTime(); StepTime <= accumulatedTime; accumulatedTime -= StepTime)
 		{
@@ -35,55 +109,9 @@ void Main()
 			world.update(StepTime);
 
 			// 地面の下に 500 cm 以上落下した物体を削除する
-			bodies.remove_if([](const P2Body& body) { return (500 < body.getPos().y); });
+			// bodies.remove_if([](const P2Body& body) { return (500 < body.getPos().y); });
 		}
-
-		// 2D カメラを更新する
-		camera.update();
-		{
-			// 2D カメラから Transformer2D を作成する
-			const auto t = camera.createTransformer();
-
-			// すべての地面を描画する
-			for (const auto& ground : grounds)
-			{
-				ground.draw(Palette::Gray);
-			}
-
-			// すべてのボディを描画する
-			for (const auto& body : bodies)
-			{
-				body.draw(HSV{ body.id() * 10.0 });
-			}
-		}
-
-		// 2D カメラの操作を描画する
-		camera.draw(Palette::Orange);
-
-		if (SimpleGUI::Button(U"Circle", Vec2{ 40, 80 }, 120))
-		{
-			bodies << world.createCircle(P2Dynamic, Vec2{ Random(-400, 400), -600 }, 20);
-		}
-
-		if (SimpleGUI::Button(U"Rect", Vec2{ 40, 120 }, 120))
-		{
-			bodies << world.createRect(P2Dynamic, Vec2{ Random(-400, 400), -600}, Size{20, 60});
-		}
-
-		if (SimpleGUI::Button(U"Triangle", Vec2{ 40, 160 }, 120))
-		{
-			bodies << world.createTriangle(P2Dynamic, Vec2{ Random(-400, 400), -600 }, Triangle{ 40 });
-		}
-
-		if (SimpleGUI::Button(U"Quad", Vec2{ 40, 200 }, 120))
-		{
-			bodies << world.createQuad(P2Dynamic, Vec2{ Random(-400, 400), -600 }, RectF{ Arg::center(0, 0), 40 }.skewedX(45_deg) );
-		}
-
-		if (SimpleGUI::Button(U"Polygon", Vec2{ 40, 240 }, 120))
-		{
-			const Polygon polygon = Shape2D::NStar(5, 30, 20);
-			bodies << world.createPolygon(P2Dynamic, Vec2{ Random(-400, 400), -600 }, polygon);
-		}
+		m_player.Update();
+		m_player.Draw(grounds);
 	}
 }
