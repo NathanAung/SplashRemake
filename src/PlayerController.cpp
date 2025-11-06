55db3767da49bfe6ae1a4ce03ea6784c1e8cc8e9
hakesi6221 <yuanjianren000@gmail.com>
2025-11-06 22:59:34 +0900

    Update Player

--- src/PlayerController.cpp
+++ src/PlayerController.cpp
@@ -1,40 +1,50 @@
 #include "PlayerController.h"
 
-void PlayerController::MoveHorizontal(Vec2* velocity)
+void PlayerController::MoveHorizontal(Vec2 velocity)
 {
 	switch (m_state){
 		case Liquid:
 			if (KeyD.pressed()){
-				m_collider.setVelocity( Vec2{ 1 * m_moveSpeed, velocity->y } );
+				m_collider.setVelocity( Vec2{ 1 * m_moveSpeed, velocity.y } );
 			}
 			if (KeyA.pressed()){
-				m_collider.setVelocity( Vec2{ -1 * m_moveSpeed, velocity->y } );
+				m_collider.setVelocity( Vec2{ -1 * m_moveSpeed, velocity.y } );
 			}
-			if (KeyD.up() || KeyA.up()){
-				m_collider.setVelocity( Vec2{ 0 , velocity->y } );
+			if (!KeyD.pressed() && !KeyA.pressed()){
+				m_collider.setVelocity( Vec2{ 0 , velocity.y } );
 			}
 			break;
 		case Solid:
 			if (KeyD.pressed()){
 				m_collider.applyForce( Vec2{ 1 * m_addForceOnSolid, 0 } );
-				velocity->x = Clamp(velocity->x, m_moveSpeed * -1, m_moveSpeed);
-				m_collider.setVelocity(*velocity);
+				velocity.x = Clamp(velocity.x, m_moveSpeed * -1, m_moveSpeed);
 			}
 			if (KeyA.pressed()){
 				m_collider.applyForce( Vec2{ -1 * m_addForceOnSolid, 0 } );
-				velocity->x = Clamp(velocity->x, m_moveSpeed * -1, m_moveSpeed);
-				m_collider.setVelocity(*velocity);
+				velocity.x = Clamp(velocity.x, m_moveSpeed * -1, m_moveSpeed);
 			}
+			if (!KeyD.pressed() && !KeyA.pressed()){
+				if (velocity.x > 1){
+					m_collider.applyForce( Vec2{ -1 * m_addForceOnSolid, 0 } );
+				}
+				else if (velocity.x < -1){
+					m_collider.applyForce( Vec2{ m_addForceOnSolid, 0 } );
+				}
+				else{
+					velocity.x = 0;
+				}
+			}
+			m_collider.setVelocity(velocity);
 			break;
 		case Gas:
 			if (KeyD.pressed()){
-				m_collider.setVelocity( Vec2{ 1 * m_moveSpeed * 0.5, velocity->y } );
+				m_collider.setVelocity( Vec2{ 1 * m_moveSpeed * 0.5, m_floatSpeed * -1 } );
 			}
 			if (KeyA.pressed()){
-				m_collider.setVelocity( Vec2{ -1 * m_moveSpeed * 0.5, velocity->y } );
+				m_collider.setVelocity( Vec2{ -1 * m_moveSpeed * 0.5, m_floatSpeed * -1 } );
 			}
-			if (KeyD.up() || KeyA.up()){
-				m_collider.setVelocity( Vec2{ 0 , velocity->y } );
+			if (!KeyD.pressed() && !KeyA.pressed()){
+				m_collider.setVelocity( Vec2{ 0 , m_floatSpeed * -1 } );
 			}
 			break;
 		default:
@@ -75,7 +85,7 @@ void PlayerController::Float(Vec2 velocity)
 
 void PlayerController::Input()
 {
-	MoveHorizontal(&m_collider.getVelocity());
+	MoveHorizontal(m_collider.getVelocity());
 	if (Key1.down())
 		ChangeLiquid();
 	else if (Key2.down())
@@ -87,35 +97,76 @@ void PlayerController::Input()
 PlayerController::PlayerController(P2World* world, Vec2 firstPos)
 {
 	m_state = Liquid;
-	m_world = world;
 	m_sprite = m_textureLiquid;
-	m_collider = m_world->createRect(P2Dynamic, firstPos, m_collSize, P2Material{ 1.0f, 0, 0, 1.0f});
+	m_collider = world->createRect(P2Dynamic, firstPos, m_collSize, P2Material{ 1.0f, 0, 0, 1.0f});
 	m_collider.setFixedRotation(true);
 }
 
 PlayerController::~PlayerController()
 {
-	delete m_world;
+
 }
 
-void PlayerController::Update()
+void PlayerController::Update(double deltaTime)
 {
 	// m_position = m_collider.getPos();
+	if (m_state != Liquid){
+		m_ep = Max(m_ep - deltaTime * m_epDecreaseSpeedPerSec, 0.0);
+		if (m_ep <= 0)
+			ChangeLiquid();
+	}
+	else
+		m_ep = Min(m_ep + deltaTime * m_epIncreaseSpeedPerSec, m_maxEP);
 	Print << U"State " << m_state << U"\n";
+	Print << U"EP： " << m_ep << U"\n";
 	if (m_collider.getVelocity().x > 0)
 		m_flipSprite = false;
 	else if (m_collider.getVelocity().x < 0)
 		m_flipSprite = true;
+	// Float(m_collider.getVelocity());
 	Input();
-	Float(m_collider.getVelocity());
 }
 
 void PlayerController::Draw()
 {
-	m_sprite(0, 0, 960, 960).resized(200).mirrored(m_flipSprite).drawAt(m_collider.getPos());
+	int cellSize = 0;
+	switch (m_state){
+		case Liquid:
+			cellSize = m_liquidCellSize;
+			break;
+		case Solid:
+			cellSize = m_solidCellSize;
+			break;
+		case Gas:
+			cellSize = m_gasCellSize;
+			break;
+		default:
+			break;
+	}
+	m_sprite(0, 0, cellSize, cellSize).resized(200).mirrored(m_flipSprite).drawAt(m_collider.getPos());
+}
+
+State PlayerController::GetState()
+{
+    return m_state;
 }
 
 P2Body* PlayerController::GetBody()
 {
     return &m_collider;
+}
+
+double PlayerController::HP()
+{
+    return m_hp;
+}
+
+void PlayerController::OnDamage(double damage)
+{
+	m_hp = Max(m_hp - damage, 0.0);
+}
+
+double PlayerController::EP()
+{
+    return m_ep;
 }
--- src/PlayerController.h
+++ src/PlayerController.h
@@ -2,32 +2,40 @@
 
 # include <Siv3D.hpp>
 
+enum State{
+		None,		// 例外用
+		Solid,		// 固体
+		Liquid,		// 液体
+		Gas,		// 気体
+	};
+
 class PlayerController{
 private:
-	enum State{
-			None,		// 例外用
-			Solid,		// 固体
-			Liquid,		// 液体
-			Gas,		// 気体
-		};
-	const double m_moveSpeed = 300;		// 液体時：移動速度
-	const double m_addForceOnSolid = 450;	// 固体時：横移動加速度
-	const double m_floatSpeed = 200;	// 気体時：上昇速度
-	const double m_maxHP = 100;			// 最大HP
+	const double m_moveSpeed = 300;				// 液体時：移動速度
+	const double m_addForceOnSolid = 450;		// 固体時：横移動加速度
+	const double m_floatSpeed = 200;			// 気体時：上昇速度
+	const double m_maxHP = 100;					// 最大HP
+	const double m_maxEP = 100;					// 最大EP
+	const double m_epDecreaseSpeedPerSec = 20;	// EPの減少速度
+	const double m_epIncreaseSpeedPerSec = 5;	// EPの上昇速度
 	const Size m_collSize{ 100, 100 };	// 当たり判定のサイズ
-	P2World* m_world;		// P2ワールド
-	P2Body m_collider;		// 物理物体の当たり判定
-	// 液体時のテクスチャ
 	const Texture m_textureLiquid{ U"../App/Assets/Sprites/Player/pl_idle.png" };
+	const int m_liquidCellSize = 960;
 	// 固体時のテクスチャ
 	const Texture m_textureSolid{ U"../App/Assets/Sprites/Player/ps_move.png" };
+	const int m_solidCellSize = 1920;
 	// 気体時のテクスチャ
 	const Texture m_textureGas{ U"../App/Assets/Sprites/Player/pg_idle.png" };
+	const int m_gasCellSize = 384;
+	P2Body m_collider;		// 物理物体の当たり判定
+	// 液体時のテクスチャ
 	Texture m_sprite;				// 現在のスプライト
 	State m_state;
 	bool m_flipSprite = false;		// スプライトを反転するか
+	double m_hp = m_maxHP;			// 現在のHP
+	double m_ep = m_maxEP;			// 現在のEP
 
-	void MoveHorizontal(Vec2* velocity);
+	void MoveHorizontal(Vec2 velocity);
 
 	void ChangeLiquid();
 
@@ -44,9 +52,17 @@ public:
 
 	~PlayerController();
 
-	void Update();
+	void Update(double deltaTime);
 
 	void Draw();
 
+	State GetState();
+
 	P2Body* GetBody();
+
+	double HP();
+
+	void OnDamage(double damage);
+
+	double EP();
 };
