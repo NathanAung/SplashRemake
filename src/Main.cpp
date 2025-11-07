d2f49e40dfdc5f8276b48c5c7b8fe997d79e7d15
Kiliken <francescopaolomariani7@gmail.com>
2025-11-07 18:28:24 +0900

    Merged Player Script

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,13 @@
 ﻿# include "PlayerController.h"
 # include "Camera.h"
+# include "Enemy.h"
+# include "MeleeEnemy.h"
 
 void Main()
 {
 	Window::Resize(1280, 720);
+
+	TextureAsset::Register(U"test",U"Assets/Sprites/Enemy/Melee/enemySprite.png");
 
 	// 2D 物理演算のシミュレーションステップ（秒）
 	constexpr double StepTime = (1.0 / 200.0);
@@ -22,6 +26,11 @@ void Main()
 	Array<P2Body> grounds;
 	grounds << world.createRect(P2Static, Vec2{ m_firstPos.x, m_firstPos.y + 300 }, SizeF{ 2000, 20 });
 	grounds << world.createRect(P2Static, Vec2{ m_firstPos.x, m_firstPos.y + 100 }, SizeF{ 100, 20 });
+
+	Array<MeleeEnemy> enemies;
+	enemies << MeleeEnemy(&world, U"test", Vec2{ 200, -600 }, 50);
+	enemies << MeleeEnemy(&world, U"test", Vec2{ 0, -600 }, 100);
+
 	// grounds << world.createLine(P2Static, Vec2{ 0, 0 }, Line{ -500, -150, -300, -50 });
 	// grounds << world.createLineString(P2Static, Vec2{ 0, 0 }, LineString{ Vec2{ 100, -50 }, Vec2{ 200, -50 }, Vec2{ 600, -150 } });
 
@@ -34,12 +43,19 @@ void Main()
 		{
 			// 2D 物理演算のワールドを StepTime 秒進める
 			world.update(StepTime);
+			
 
+			for (auto& enemy : enemies)
+			{
+				enemy.Update(StepTime);
+			}
 			// 地面の下に 500 cm 以上落下した物体を削除する
 			// bodies.remove_if([](const P2Body& body) { return (500 < body.getPos().y); });
 		}
-		m_player.Update();
+		m_player.Update(StepTime);
 		camera.Update();
 		camera.Draw(grounds);
+		
+			
 	}
 }
